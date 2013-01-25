bd9041c863c9ce065190c46e122212522a720229
Jarad Niemi <niemi@iastate.edu>
2013-01-25 10:03:54 -0600

    made int argument const

--- src/utility.c
+++ src/utility.c
@@ -4,7 +4,7 @@
 #include "utility.h"
 
 /* Utility function to determine if any element in a vector is negative */
-int anyNegative(int n, const int *v)                     // both should be const
+int anyNegative(const int n, const int *v)                   
 {
     int i;
     for (i=0; i<n; i++) 
--- src/utility.h
+++ src/utility.h
@@ -2,6 +2,6 @@
 #ifndef __UTILITY_H__ // utility.h guard
 #define __UTILITY_H__
 
-int anyNegative(int n, const int *v);                     
+int anyNegative(const int n, const int *v);                     
 
 #endif                // utility.h guard
