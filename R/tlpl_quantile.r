a258971ed1b5403a151301a3e86e027c967e19d4
Jarad Niemi <niemi@iastate.edu>
2013-02-08 09:39:40 -0600

    indenting only 2 spaces

--- R/tlpl_quantile.r
+++ R/tlpl_quantile.r
@@ -1,35 +1,35 @@
 # Calculates the quantiles for a mixture of beta distributions
 mix.beta.quantiles = function(a,b,probs,w=1)
 {
-    n = length(a)
-    p = length(probs)
-    q = rep(NA,p)
-    if (w==1) w = rep(1/n,n)
+  n = length(a)
+  p = length(probs)
+  q = rep(NA,p)
+  if (w==1) w = rep(1/n,n)
 
-    for (i in 1:length(probs)) 
-    {
-        tmp = uniroot(function(x) probs[i]-sum(w*pbeta(x,a,b)), c(0,1))
-        q[i] = tmp$root
-    }
+  for (i in 1:length(probs)) 
+  {
+    tmp = uniroot(function(x) probs[i]-sum(w*pbeta(x,a,b)), c(0,1))
+    q[i] = tmp$root
+  }
 
-    return(q)
+  return(q)
 }
 
 # Calculates the quantiles for a mixture of beta distributions
 mix.gamma.quantiles = function(a,b,probs,w=1)
 {
-    n = length(a)
-    p = length(probs)
-    q = rep(NA,p)
-    if (w==1) w = rep(1/n,n)
+  n = length(a)
+  p = length(probs)
+  q = rep(NA,p)
+  if (w==1) w = rep(1/n,n)
 
-    for (i in 1:length(probs)) 
-    {
-        tmp = uniroot(function(x) probs[i]-sum(w*pgamma(x,a,b)), c(0,10))
-        q[i] = tmp$root
-    }
+  for (i in 1:length(probs)) 
+  {
+    tmp = uniroot(function(x) probs[i]-sum(w*pgamma(x,a,b)), c(0,10))
+    q[i] = tmp$root
+  }
 
-    return(q)
+  return(q)
 }
 
 
@@ -45,42 +45,39 @@ mix.gamma.quantiles = function(a,b,probs,w=1)
 #'
 tlpl_quantile = function(tlpl, probs=c(.025,.5,.975), which="xpr", verbose=1)
 {
-    which = tolower(which)
-    n = dim(tlpl$X)[3]
-    s = dim(tlpl$X)[1]
-    p = length(probs)
+  which = tolower(which)
+  n = dim(tlpl$X)[3]
+  s = dim(tlpl$X)[1]
+  p = length(probs)
 
-    do =list()
-    do$x = grepl("x", which)
-    do$p = grepl("p", which)
-    do$r = grepl("r", which)
+  do =list(x = grepl("x", which), p = grepl("p", which), r = grepl("r", which))
  
-    if (do$p || do$r) { r = dim(tlpl$hyper$prob$a)[1] }
+  if (do$p || do$r) { r = dim(tlpl$hyper$prob$a)[1] }
  
-    if (do$x) { X.quantiles = array(NA, dim=c(s,p,n)) } else { X.quantiles=NULL }
-    if (do$p) { p.quantiles = array(NA, dim=c(r,p,n)) } else { p.quantiles=NULL }
-    if (do$r) { r.quantiles = array(NA, dim=c(r,p,n)) } else { r.quantiles=NULL }
+  if (do$x) { X.quantiles = array(NA, dim=c(s,p,n)) } else { X.quantiles=NULL }
+  if (do$p) { p.quantiles = array(NA, dim=c(r,p,n)) } else { p.quantiles=NULL }
+  if (do$r) { r.quantiles = array(NA, dim=c(r,p,n)) } else { r.quantiles=NULL }
 
-    for (i in 1:n) 
-    {   
-        if (verbose) cat(paste("Time point ",i,", ",round(i/n*100), "% completed.\n", sep=''))
+  for (i in 1:n) 
+  {   
+    if (verbose) cat(paste("Time point ",i,", ",round(i/n*100), "% completed.\n", sep=''))
 
-        if (do$x) 
-        {
-        	for (j in 1:s) X.quantiles[j,,i] = quantile(tlpl$X[j,,i], probs=probs)
-        }
-
-        if (do$p) 
-        {
-        	for (j in 1:r) p.quantiles[j,,i] = mix.beta.quantiles( tlpl$hyper$prob$a[j,,i], tlpl$hyper$prob$b[j,,i], probs)
-        }
-        
-        if (do$r)
-        {
-        	for (j in 1:r) r.quantiles[j,,i] = mix.gamma.quantiles(tlpl$hyper$rate$a[j,,i], tlpl$hyper$rate$b[j,,i], probs)
-        }
+    if (do$x) 
+    {
+      for (j in 1:s) X.quantiles[j,,i] = quantile(tlpl$X[j,,i], probs=probs)
     }
 
-    return(list(X.quantiles=X.quantiles, p.quantiles=p.quantiles, r.quantiles=r.quantiles))
+    if (do$p) 
+    {
+      for (j in 1:r) p.quantiles[j,,i] = mix.beta.quantiles( tlpl$hyper$prob$a[j,,i], tlpl$hyper$prob$b[j,,i], probs)
+    }
+        
+    if (do$r)
+    {
+      for (j in 1:r) r.quantiles[j,,i] = mix.gamma.quantiles(tlpl$hyper$rate$a[j,,i], tlpl$hyper$rate$b[j,,i], probs)
+    }
+  }
+
+  return(list(X.quantiles=X.quantiles, p.quantiles=p.quantiles, r.quantiles=r.quantiles))
 }
 
