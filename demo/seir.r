6cbcdca45c641f0b04c0fd83d394e1da016df7be
Jarad Niemi <niemi@iastate.edu>
2013-08-21 17:13:17 -0500

    updated seir demo to use sckm function call

--- demo/seir.r
+++ demo/seir.r
@@ -3,24 +3,14 @@ ask   = TRUE
 
 # Generate data
 ## Set up SIR model
-sckm = list()
-sckm$s = 4 # species (S,E,I,R)
-sckm$r = 3 # reactions (S->I, I->R)
-#                    S -> E      E -> I      I -> R
-sckm$Pre  = rbind( c(1,0,1,0), c(0,1,0,0), c(0,0,1,0))
-sckm$Post = rbind( c(0,1,1,0), c(0,0,1,0), c(0,0,0,1))
-sckm$stoich = t(sckm$Post-sckm$Pre)
-sckm$X = c(16000,0,100,0)
-N = sum(sckm$X)
-sckm$theta = c(0.5,0.25,0.25)
-sckm$mult = c(1/N,1,1)
+seir = sckm("seir", X=c(16000,0,100,0), theta=c(.5,.25,.25))
 
 ## Simulate data
 set.seed(2)
 n = 100
 
 ### True states and transitions
-tl = tau_leap(sckm, n)
+tl = tau_leap(seir, n)
 
 ### Sample transitions
 p = c(0,.05,0) # Sample probabilities for S->I, E->I, and I->R respectively
@@ -32,23 +22,18 @@ y = cbind(rbinom(n, tl$nr[,1], p[1]),
 clrs = c("seagreen","pink","red","blue")
 ld=3
 par(mfrow=c(1,3))
-plot( tl$X[,1], type="l", ylim=c(0,sckm$X[1]), lwd=ld, col=clrs[1], 
+plot( tl$X[,1], type="l", ylim=c(0,seir$X[1]), lwd=ld, col=clrs[1], 
       xlab="Time", ylab="Number", main="Truth")
 lines(tl$X[,2], lwd=ld, col=clrs[2])
 lines(tl$X[,3], lwd=ld, col=clrs[3])
 lines(tl$X[,4], lwd=ld, col=clrs[4])
 legend("topright", c("S","E","I","R"), col=clrs, lwd=ld)
 
-
-
-
 plot( y[,1], type="p", pch=19, ylim=range(y), lwd=ld, col=clrs[2], 
       xlab="Time", ylab="Number", main="Observations")
 points(y[,2], lwd=ld, col=clrs[3], pch=19)
 points(y[,3], lwd=ld, col=clrs[4], pch=19)
 legend("topright", c("S->E","E->I","I->R"), col=clrs[2:4], lwd=ld)
-
-
 
 ### Cumulative transitions
 y2 = apply(y,2,cumsum)
@@ -66,8 +51,8 @@ if (ask) readline("Hit <enter> to continue:")
 
 # Perform inference
 cat("\nRunning sequential inference...\n")
-prior = tlpl_prior(sckm$X, 1e6*(p+.0001), 1e6*(1-(p+.0001)), sckm$theta*1e6, 1e6, sckm$r)
-z = tlpl(list(y=y, tau=1), sckm, prior=prior, n.particles=1e4, engine="C", verbose=1)
+prior = tlpl_prior(seir$X, 1e6*(p+.0001), 1e6*(1-(p+.0001)), seir$theta*1e6, 1e6, seir$r)
+z = tlpl(list(y=y, tau=1), seir, prior=prior, n.particles=1e4, engine="C", verbose=1)
 
 cat("\nCalculating quantiles...\n")
 qs = tlpl_quantile(z)
@@ -83,26 +68,26 @@ xx = 0:n
 par(mfrow=c(2,2))
 
 ### Susceptible
-plot( xx, qs$X[1,1,], type="l", ylim=c(0, max(sckm$X)), main="Susceptible", ylab="Count", xlab="Time")
+plot( xx, qs$X[1,1,], type="l", ylim=c(0, max(seir$X)), main="Susceptible", ylab="Count", xlab="Time")
 lines(xx, qs$X[1,2,], lwd=ld)
 lines(xx, qs$X[1,3,])
 lines(xx, tl$X[,1], col="red", lwd=ld)
 
 ### Exposed
-plot( xx, qs$X[2,1,], type="l", ylim=c(0, max(sckm$X)), main="Exposed", ylab="Count", xlab="Time")
+plot( xx, qs$X[2,1,], type="l", ylim=c(0, max(seir$X)), main="Exposed", ylab="Count", xlab="Time")
 lines(xx, qs$X[2,2,], lwd=ld)
 lines(xx, qs$X[2,3,])
 lines(xx, tl$X[,2], col="red", lwd=ld)
 
 ### Infected
-plot( xx, qs$X[3,1,], type="l", ylim=c(0, max(sckm$X)), main="Infected", ylab="Count", xlab="Time")
+plot( xx, qs$X[3,1,], type="l", ylim=c(0, max(seir$X)), main="Infected", ylab="Count", xlab="Time")
 lines(xx, qs$X[3,2,], lwd=ld)
 lines(xx, qs$X[3,3,])
 lines(xx, tl$X[,3], col="red", lwd=ld)
 
 
 ### Recovered
-plot( xx, qs$X[4,1,], type="l", ylim=c(0, max(sckm$X)), main="Recovered", ylab="Count", xlab="Time")
+plot( xx, qs$X[4,1,], type="l", ylim=c(0, max(seir$X)), main="Recovered", ylab="Count", xlab="Time")
 lines(xx, qs$X[4,2,], lwd=ld)
 lines(xx, qs$X[4,3,])
 lines(xx, tl$X[,4], col="red", lwd=ld)
@@ -143,21 +128,21 @@ abline(h=p[3], col="red", lwd=ld)
 plot( xx, qs$r[1,1,], type="l", ylim=range(qs$r[1,,]), main="S -> E", ylab="Rate", xlab="Time")
 lines(xx, qs$r[1,2,], lwd=ld)
 lines(xx, qs$r[1,3,])
-abline(h=sckm$theta[1], col="red", lwd=ld)
+abline(h=seir$theta[1], col="red", lwd=ld)
 
 
 ### E->I rate
 plot( xx, qs$r[2,1,], type="l", ylim=range(qs$r[2,,]), main="E -> I", ylab="Rate", xlab="Time")
 lines(xx, qs$r[2,2,], lwd=ld)
 lines(xx, qs$r[2,3,])
-abline(h=sckm$theta[2], col="red", lwd=ld)
+abline(h=seir$theta[2], col="red", lwd=ld)
 
 
 ### I->R rate
 plot( xx, qs$r[3,1,], type="l", ylim=range(qs$r[3,,]), main="I -> R", ylab="Rate", xlab="Time")
 lines(xx, qs$r[3,2,], lwd=ld)
 lines(xx, qs$r[3,3,])
-abline(h=sckm$theta[3], col="red", lwd=ld)
+abline(h=seir$theta[3], col="red", lwd=ld)
 
 
 
@@ -176,7 +161,7 @@ z2$hyper$rate = list(a = z$hyper$rate$a[,1:np,tt],
 z2$hyper$prob = list(a = z$hyper$prob$a[,1:np,tt], 
                      b = z$hyper$prob$b[,1:np,tt])
 
-z3 = tlpl_predict(sckm, n-tt, z2, verbose=1)
+z3 = tlpl_predict(seir, n-tt, z2, verbose=1)
 
 Xq = tlpl_quantile(z3, which="x")
 
@@ -188,7 +173,7 @@ x1 = xx[1:tt]
 x2 = xx[tt:n]
 
 ### Susceptible
-plot( x1, qs$X[1,1,1:tt], type="l", ylim=c(0, max(sckm$X)), xlim=c(0,n), main="Susceptible", ylab="Count", xlab="Time")
+plot( x1, qs$X[1,1,1:tt], type="l", ylim=c(0, max(seir$X)), xlim=c(0,n), main="Susceptible", ylab="Count", xlab="Time")
 lines(x1, qs$X[1,2,1:tt], lwd=ld)
 lines(x1, qs$X[1,3,1:tt])
 
@@ -200,7 +185,7 @@ lines(xx, tl$X[,1], col="red", lwd=ld)
 
 
 ### Exposed
-plot( x1, qs$X[2,1,1:tt], type="l", ylim=c(0, max(sckm$X)), xlim=c(0,n), main="Exposed", ylab="Count", xlab="Time")
+plot( x1, qs$X[2,1,1:tt], type="l", ylim=c(0, max(seir$X)), xlim=c(0,n), main="Exposed", ylab="Count", xlab="Time")
 lines(x1, qs$X[2,2,1:tt], lwd=ld)
 lines(x1, qs$X[2,3,1:tt])
 
@@ -212,7 +197,7 @@ lines(xx, tl$X[,2], col="red", lwd=ld)
 
 
 ### Infected
-plot( x1, qs$X[3,1,1:tt], type="l", ylim=c(0, max(sckm$X)), xlim=c(0,n), main="Infected", ylab="Count", xlab="Time")
+plot( x1, qs$X[3,1,1:tt], type="l", ylim=c(0, max(seir$X)), xlim=c(0,n), main="Infected", ylab="Count", xlab="Time")
 lines(x1, qs$X[3,2,1:tt], lwd=ld)
 lines(x1, qs$X[3,3,1:tt])
 
@@ -224,7 +209,7 @@ lines(xx, tl$X[,3], col="red", lwd=ld)
 
 
 ### Recovered
-plot( x1, qs$X[4,1,1:tt], type="l", ylim=c(0, max(sckm$X)), xlim=c(0,n), main="Recovered", ylab="Count", xlab="Time")
+plot( x1, qs$X[4,1,1:tt], type="l", ylim=c(0, max(seir$X)), xlim=c(0,n), main="Recovered", ylab="Count", xlab="Time")
 lines(x1, qs$X[4,2,1:tt], lwd=ld)
 lines(x1, qs$X[4,3,1:tt])
 
