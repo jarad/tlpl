f5d4a09f208170618e796cfae45f67ebe12751e4
Jarad Niemi <niemi@iastate.edu>
2013-08-29 21:06:40 -0500

    added those wrapper files

---
+++ src/resample_R.h
@@ -0,0 +1,17 @@
+
+void is_increasing_R(int *, const double *, int*);
+void cumulative_sum_R(int *, double *);
+void rep2id_R(int *, int *, int *);
+void inverse_cdf_weights_R(int *, double *, int *, double *, int *);
+
+void ess_R(int *, double *, double *);
+void cov2_R(int *, double *, double *);
+void entropy_R(int *, double *, double *);
+
+void resample_R(int *, double *, int *, int *, int *);
+
+void multinomial_resample_R(int *, double *, int *, int *);
+void stratified_resample_R(int *, double *, int *, int *);
+void systematic_resample_R(int *, double *, int *, int *);
+void residual_resample_R(int *, double *, int *, int *, int *);
+
