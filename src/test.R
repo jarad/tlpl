b789cc861b15e89617d2823072e7846ef52e1e50
Jarad Niemi <niemi@iastate.edu>
2016-04-14 17:04:40 -0500

    Add update_species function

--- src/test.R
+++ src/test.R
@@ -1,4 +1,15 @@
 library(Rcpp)
 
-sourceCpp("src/anyNegative.cpp")
 sourceCpp("src/resample.cpp")
+
+resample(runif(5),10, 2)
+
+
+sourceCpp("src/gillespie.cpp")
+
+# SIR
+A = cbind(c(-1,1,0), c(0,-1,1))
+S = t(A)
+
+update_species(S, c(1,1), c(2,3,0))
+
