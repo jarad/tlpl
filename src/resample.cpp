7e37b2d291a2e0cc3e1293d25d1aeeb4c795c883
Jarad Niemi <niemi@iastate.edu>
2016-04-14 16:38:56 -0500

    Add resample function
    The switch statement currently works on integers defined at the top of the file.
    But it would be easier to read if it worked on a string, e.g. 'multinomial'.

--- src/resample.cpp
+++ src/resample.cpp
@@ -5,6 +5,14 @@
 using namespace Rcpp;
 
 
+
+#define STRATIFIED_RESAMPLING 1
+#define MULTINOMIAL_RESAMPLING 2
+#define SYSTEMATIC_RESAMPLING 3
+#define RESIDUAL_RESAMPLING_THEN_STRATIFIED 4
+#define RESIDUAL_RESAMPLING_THEN_MULTINOMIAL 5
+#define RESIDUAL_RESAMPLING_THEN_SYSTEMATIC 6
+
 double ess(NumericVector w)
 {
   NumericVector w2 = w*w;
@@ -12,10 +20,41 @@ double ess(NumericVector w)
 }
 
 
-
-// [[Rcpp::export]]
 IntegerVector multinomial_resample(NumericVector w, int n)
 {
   IntegerVector ii = seq_len(w.length());
   return RcppArmadillo::sample(ii, n, TRUE, w);
 }
+
+
+// [[Rcpp::export]]
+IntegerVector resample(NumericVector w, int n, int method)
+{
+  switch(method)
+  {
+//    case STRATIFIED_RESAMPLING:
+//      stratified_resample(nW, adWeights, nI, anIndices);
+//      break;
+    case MULTINOMIAL_RESAMPLING:
+      return multinomial_resample(w, n);
+      break;
+//    case SYSTEMATIC_RESAMPLING:
+//      systematic_resample(nW, adWeights, nI, anIndices);
+//      break;
+//    case RESIDUAL_RESAMPLING_THEN_STRATIFIED:
+//      residual_resample(nW, adWeights, nI, anIndices, STRATIFIED_RESAMPLING);
+//      break;
+//    case RESIDUAL_RESAMPLING_THEN_MULTINOMIAL: 
+//      residual_resample(nW, adWeights, nI, anIndices, MULTINOMIAL_RESAMPLING);
+//      break;
+//    case RESIDUAL_RESAMPLING_THEN_SYSTEMATIC : 
+//      residual_resample(nW, adWeights, nI, anIndices, SYSTEMATIC_RESAMPLING);
+//      break;
+    default:
+      throw std::range_error("resample: no match for resampling function");
+  }
+}
+
+
+
+
