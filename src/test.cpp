d962713654c51eeeae1a88f31497b3a4e535d1ba
Jarad Niemi <niemi@iastate.edu>
2016-04-15 17:42:36 -0500

    Update test file

--- src/test.cpp
+++ src/test.cpp
@@ -4,12 +4,13 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 void test(List sckm_list) {
+  
   Sckm sckm(sckm_list);
   
   int nr = sckm.n_reactions(); 
   IntegerVector reaction_count = as<IntegerVector>(rpois(nr,1));
   
-  Rcout << nr << std::endl;
+  sckm.printX();
   sckm.update(reaction_count);
-//  sckm.print();
+  sckm.printX();
 }
